2c68cb847459ce21bbad47e9dd220f0889524ff6
Fellah Faizel <147988219+fellahfaizel@users.noreply.github.com>
2025-09-23 20:10:13 +0530

    Update profile.go

--- backend/routes/profile.go
+++ backend/routes/profile.go
@@ -13,8 +13,9 @@ func GetProfile(c *gin.Context) {
 		"bio": `I’m a final-year Computer Science Engineering student (graduating in 2026) at Government College of Engineering, Kannur.
 Currently working as a backend developer, I specialize in building intelligent systems and APIs using Python, Go, and modern databases like Neo4j and MongoDB.
 With a strong foundation in Computer Science, I’m now exploring Machine Learning and Deep Learning to create systems that learn, adapt, and solve real-world problems.`,
-		"imageUrl": "http://localhost:8080/static/profile.jpg", // later this can come from static hosting or DB
+		"imageUrl": "https://fellah-backend.onrender.com/static/profile.jpg", // later this can come from static hosting or DB
 	}
 
 	c.JSON(http.StatusOK, profile)
 }
+
